8d938573133bd01357f6f5820ced8fdb590b7542
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-19 06:41:24 +0000

    Add split calculation

--- main.py
+++ main.py
@@ -17,13 +17,23 @@ tip_request = input('Do you want to tip? Yes or No\n').title()
 if tip_request == 'Yes':
     TIP = int(input("How much tip percentage do you want to add? e.g. 5, 10 or 15 \n"))
 elif tip_request == 'No':
-    print('No tip')
+    TIP = 0
 else:
     print('Invalid input')
 
 calc_tip = (amount * (TIP / 100))
-print(f"{calc_tip:.2f}")
 bill = amount + calc_tip
 
-print(bill)
+# splitting the bill by how many people?
+split_bill_num = int(input("How many people are splitting the bill?\n"))
+
+if split_bill_num > 1:
+    bill_per_person = bill / split_bill_num
+    print(f"Your new total: {bill:.2f}\nTip: {calc_tip:.2f}\nBill Per Person: {bill_per_person:.2f}\n")
+elif split_bill_num == 1:
+    print(f"Your new total is: {bill:.2f}\nTip: {calc_tip:.2f}\n ")
+else:
+    print('Invalid input')
+
+print("Thanks for using the application\n")
 
