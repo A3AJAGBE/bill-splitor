ff776e6565b46328d948cb4ef342460b7fa8c0ff
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-19 05:42:03 +0000

    Add logo

--- main.py
+++ main.py
@@ -7,21 +7,9 @@
 This application calculates tip and assist in spliting bills.
 """
 
-print("Welcome to A3AJAGBE TipCalc\n")
+from logo import logo
+print(logo)
 
-amount = float(input("What is your bill total?\n"))
 
-tip = int(input("How much tip percentage do you want to add? e.g. 5, 10 or 15 \n"))
-
-calc_tip = (amount * (tip / 100))
-
-total_share = int(input("How many people will be sharing the bill?\n"))
-
-bill = amount + calc_tip
-bill_split = bill / total_share
-
-print(f"Your new total is: {bill:.2f}, and the split per person is: {bill_split:.2f}\n")
-
-print("Thanks for using the application!!!\n")
 
 
