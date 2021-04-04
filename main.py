7e40535ecc2299481d3c565016cc39a673a15708
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-04 08:39:10 +0100

    Add validation to prevent negative inputs

--- main.py
+++ main.py
@@ -17,27 +17,35 @@ def app_func():
         print("That's not a number. Try Again!!!\n")
         app_func()
     else:
-        # bill = format(amount, '.2f')
-
-        tip_request = input('(Yes or No): Do you want to tip? ').capitalize()
-
-        # Continue depending on the tip request response
-        if tip_request == 'Yes':
-            try:
-                TIP = int(input("What percentage of tip to add? e.g. 5, 10 or 15 \n"))
-            except ValueError:
-                print("That's not a number. Try Again!!!\n")
-                app_func()
-        elif tip_request == 'No':
-            TIP = 0
-        else:
-            print('Invalid input\n')
+        # Making sure amount is not zero or negative number
+        if amount <= 0:
+            print('That input is not allowed.\n')
             app_func()
+        else:
+            tip_request = input('(Yes or No): Do you want to tip? ').capitalize()
 
-        # Complete the first calculation
-        calc_tip = amount * (TIP / 100)
-        bill = amount + calc_tip
-        print(f"Initial bill of {amount} with {TIP}% = {bill:.2f}")
+            # Continue depending on the tip request response
+            if tip_request == 'Yes':
+                try:
+                    TIP = int(input("What percentage of tip to add? e.g. 5, 10 or 15 \n"))
+                except ValueError:
+                    print("That's not a number. Try Again!!!\n")
+                    app_func()
+            elif tip_request == 'No':
+                TIP = 0
+            else:
+                print('Invalid input\n')
+                app_func()
+
+            # Making sure tip is not negative number
+            if TIP < 0:
+                print('That is a negative number, not allowed.\n')
+                app_func()
+            else:
+                # Complete the first calculation
+                calc_tip = amount * (TIP / 100)
+                bill = amount + calc_tip
+                print(f"Initial bill of {amount:.2f} with {TIP}% = {bill:.2f}")
 
 
 app_func()
