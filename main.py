3bdc54088b07488781b26cd2d538717cd8d32b62
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-04 09:09:00 +0100

    Add split bill function

--- main.py
+++ main.py
@@ -5,7 +5,24 @@ This application splits total bill and calculates tip.
 from logo import logo
 
 print(logo)
-print("This application calculates tip and splitting bills.\n")
+print("This application calculates tip and splitting bills.")
+print("To prevent starting again, type valid inputs only.\n")
+
+
+def split_bill(bill):
+    """This function calculates the bill split"""
+    try:
+        split_by = int(input("How many people are splitting the bill?\n"))
+    except ValueError:
+        print("Only integers allowed. Try Again!!!\n")
+        app_func()
+    else:
+        if split_by > 1:
+            per_person = bill / split_by
+            print(f"Bill to pay is {bill:.2f}\nBill Per Person: {per_person:.2f}")
+        else:
+            print('Invalid input\n')
+            app_func()
 
 
 def app_func():
@@ -29,12 +46,12 @@ def app_func():
                 try:
                     TIP = int(input("What percentage of tip to add? e.g. 5, 10 or 15 \n"))
                 except ValueError:
-                    print("That's not a number. Try Again!!!\n")
+                    print("Only integers allowed. Try Again!!!\n")
                     app_func()
             elif tip_request == 'No':
                 TIP = 0
             else:
-                print('Invalid input\n')
+                print('Invalid input.\n')
                 app_func()
 
             # Making sure tip is not negative number
@@ -45,8 +62,20 @@ def app_func():
                 # Complete the first calculation
                 calc_tip = amount * (TIP / 100)
                 bill = amount + calc_tip
-                print(f"Initial bill of {amount:.2f} with {TIP}% = {bill:.2f}")
+                print(f"\nUpdated Information\nInitial bill of {amount:.2f} with {TIP}% = {bill:.2f}")
 
+                split_request = input('(Yes or No): Do you want to split the bill? ').capitalize()
 
+                # Continue depending on the split request response
+                if split_request == 'Yes':
+                    split_bill(bill)
+                    print("Thanks for using the application")
+                elif split_request == 'No':
+                    print(f"Bill to pay is {bill:.2f}")
+                else:
+                    print('Invalid input.\n')
+                    app_func()
+
+                print("Thanks for using the application.")
 app_func()
 
